464449898cc032894d4860d8444e9f74a12c3f32
bengreenier <ben@bengreenier.com>
2017-09-01 16:06:24 -0400

    csharpifying

---
+++ Overlights/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace Overlights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = new NamedPipeClientStream(".", "RzChromaData", PipeDirection.In);
+
+            Console.Write("Connecting...");
+
+            stream.Connect();
+
+            Console.WriteLine("Connected");
+
+            using (var sr = new StreamReader(stream))
+            {
+                while (stream.IsConnected)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+
+            stream.Close();
+        }
+    }
+}
